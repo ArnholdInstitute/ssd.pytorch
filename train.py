e90c5fca552a52978f2b349d5eb861f051f437ee
ml9951 <ml9951@rit.edu>
2017-10-24 15:26:05 +0000

    Properly resume checkpoints in training

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ from utils.augmentations import SSDAugmentation
 from layers.modules import MultiBoxLoss
 from ssd import build_ssd
 from Dataset import Dataset
+from datetime import datetime
 
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
@@ -22,19 +23,21 @@ parser = argparse.ArgumentParser(description='Single Shot MultiBox Detector Trai
 parser.add_argument('--version', default='v2', help='conv11_2(v2) or pool6(v1) as last layer')
 parser.add_argument('--basenet', default='vgg16_reducedfc.pth', help='pretrained base model')
 parser.add_argument('--jaccard_threshold', default=0.5, type=float, help='Min Jaccard index for matching')
-parser.add_argument('--batch_size', default=16, type=int, help='Batch size for training')
+parser.add_argument('--batch_size', default=32, type=int, help='Batch size for training')
 parser.add_argument('--resume', default=None, type=str, help='Resume from checkpoint')
 parser.add_argument('--num_workers', default=2, type=int, help='Number of workers used in dataloading')
 parser.add_argument('--iterations', default=120000, type=int, help='Number of training iterations')
 parser.add_argument('--start_iter', default=0, type=int, help='Begin counting iterations starting from this value (should be used with resume)')
 parser.add_argument('--cuda', default=True, type=str2bool, help='Use cuda to train model')
-parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
+parser.add_argument('--lr', '--learning-rate', default=1e-4, type=float, help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
 parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
 parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
 parser.add_argument('--log_iters', default=True, type=bool, help='Print the loss at each iteration')
 parser.add_argument('--save_folder', default='weights/', help='Location to save checkpoint models')
 args = parser.parse_args()
+
+TIMESTAMP = datetime.now().isoformat()
 
 if args.cuda and torch.cuda.is_available():
     torch.set_default_tensor_type('torch.cuda.FloatTensor')
@@ -60,54 +63,45 @@ stepvalues = (50000, 100000, 120000)
 gamma = 0.1
 momentum = 0.9
 
-ssd_net = build_ssd('train', 300, num_classes)
-net = ssd_net
-
-if args.cuda:
-    # net = torch.nn.DataParallel(ssd_net)
-    cudnn.benchmark = True
-
-if args.resume:
-    print('Resuming training, loading {}...'.format(args.resume))
-    ssd_net.load_weights(args.resume)
-else:
-    vgg_weights = torch.load(args.save_folder + args.basenet)
-    print('Loading base network...')
-    ssd_net.vgg.load_state_dict(vgg_weights)
-
-def checkpoint(net, optim, checkpoint_name, epoch):
-    state_dict = net.state_dict()
-    for key in state_dict.keys():                                                                                                                                                                                
-        state_dict[key] = state_dict[key].cpu()                                                                                                                                                                  
-                                                                                                                                                                                                                 
-    torch.save({                                                                                                                                                                                                 
-        'epoch': epoch,                                                                                                                                                                                     
-        'state_dict': state_dict,                                                                                                                                                                                
-        'optimizer': optim},                                                                                                                                                                                     
-        checkpoint_name)
-
-if args.cuda:
-    net = net.cuda()
+net = build_ssd('train', 300, num_classes).cuda()
+cudnn.benchmark = True
 
 def xavier(param):
     init.xavier_uniform(param)
-
 
 def weights_init(m):
     if isinstance(m, nn.Conv2d):
         xavier(m.weight.data)
         m.bias.data.zero_()
 
-
-if not args.resume:
+if args.resume:
+    checkpoint = torch.load(args.resume)
+    print('Resuming training, loading {}...'.format(args.resume))
+    net.load_state_dict(checkpoint['state_dict'])
+    optimizer = checkpoint['optimizer']
+    args.start_iter = checkpoint['epoch']
+else:
+    vgg_weights = torch.load(args.save_folder + args.basenet)
+    print('Loading base network...')
+    net.vgg.load_state_dict(vgg_weights)
+    optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                      momentum=args.momentum, weight_decay=args.weight_decay)
     print('Initializing weights...')
     # initialize newly added layers' weights with xavier method
-    ssd_net.extras.apply(weights_init)
-    ssd_net.loc.apply(weights_init)
-    ssd_net.conf.apply(weights_init)
+    net.extras.apply(weights_init)
+    net.loc.apply(weights_init)
+    net.conf.apply(weights_init)
 
-optimizer = optim.SGD(net.parameters(), lr=args.lr,
-                      momentum=args.momentum, weight_decay=args.weight_decay)
+def checkpoint(net, optim, checkpoint_name, epoch):
+    state_dict = net.state_dict()
+    for key in state_dict.keys():                                                                                                                                                                                
+        state_dict[key] = state_dict[key].cpu()                                                                                                                                                                                         
+    torch.save({                                                                                                                                                                                                 
+        'epoch': epoch,                                                                                                                                                                                     
+        'state_dict': state_dict,                                                                                                                                                                                
+        'optimizer': optim},                                                                                                                                                                                     
+        checkpoint_name)
+
 criterion = MultiBoxLoss(num_classes, 0.5, True, 0, True, 3, 0.5, False, args.cuda)
 
 def train():
@@ -119,14 +113,12 @@ def train():
     print('Loading Dataset...')
 
     train_data = json.load(open('../data/train_data.json'))
-
-    # dataset = Dataset(SSDAugmentation(ssd_dim, means))
     dataset = Dataset('../data', train_data, transform=SSDAugmentation(ssd_dim, means)).even()
 
     epoch_size = len(dataset) // args.batch_size
     step_index = 0
     batch_iterator = None
-    data_loader = data.DataLoader(dataset, batch_size, #num_workers=args.num_workers,
+    data_loader = data.DataLoader(dataset, batch_size, num_workers=args.num_workers,
                                   shuffle=True, collate_fn=detection_collate, pin_memory=True)
 
     scheduler = ReduceLROnPlateau(optimizer, patience=500, min_lr=1e-6, verbose=True)
@@ -161,6 +153,8 @@ def train():
         loss = loss_l + loss_c
         loss.backward()
         optimizer.step()
+
+
         t1 = time.time()
         loc_loss += loss_l.data[0]
         conf_loss += loss_c.data[0]
@@ -169,11 +163,11 @@ def train():
 
         scheduler.step(loss.data[0])
 
-        if iteration % 5000 == 0 and iteration != 0:
+        if iteration % 1000 == 0 and iteration != args.start_iter:
             print('Saving state, iter:', iteration)
-            checkpoint(ssd_net, optimizer, 'weights/ssd300_0712_' + repr(iteration) + '.pth', iteration)
+            checkpoint(net, optimizer, 'weights/ssd300_0712_' + TIMESTAMP + '.pth', iteration)
 
-    checkpoint(ssd_net, optimizer, args.save_folder + '' + args.version + '.pth', iteration)
+    checkpoint(net, optimizer, args.save_folder + '' + args.version + '.pth', iteration)
 
 
 def adjust_learning_rate(optimizer, gamma, step):
