34baaea1764456d789a97518ff10f1e547980303
ml9951 <ml9951@rit.edu>
2017-10-25 19:09:15 +0000

    Add AWS credentials to run-docker script

--- run-docker.sh
+++ run-docker.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-IMG=ml9951/ssd
+IMG=ml9951/ssd2
 cd ..
 
 RM=--rm
 
+if [ -z $AWS_SECRET_ACCESS_KEY ]; then
+	eval $(awk -F ' = ' 'NF == 2 {print toupper($1)"="$2 }' < ~/.aws/credentials)
+fi
+
 nvidia-docker run -it $RM \
 	-w /workspace/ssd.pytorch \
-	-e DB_PASSWORD=password \
-	-e DB_URL=$(ifconfig | grep -E "([0-9]{1,3}\.){3}[0-9]{1,3}" | grep -v 127.0.0.1 | awk '{ print $2 }' | cut -f2 -d: | head -n1)\
+	-e "AWS_DEFAULT_REGION=us-west-2" \
+	-e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" \
+	-e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" \
 	--ipc=host \
 	-v $(pwd):/workspace \
 	$IMG /bin/bash
